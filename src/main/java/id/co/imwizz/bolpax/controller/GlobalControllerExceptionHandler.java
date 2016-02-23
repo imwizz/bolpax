0fc5ed24cc3436903ce2587303838131ac2bd491
sangbas <sangaji@imwizz.co.id>
2016-02-23 16:26:58 +0700

    remove annotation CrossOrigin (not supported in java 7)

--- src/main/java/id/co/imwizz/bolpax/controller/GlobalControllerExceptionHandler.java
+++ src/main/java/id/co/imwizz/bolpax/controller/GlobalControllerExceptionHandler.java
@@ -10,13 +10,11 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-@CrossOrigin
 public class GlobalControllerExceptionHandler {
 	
 	@ExceptionHandler
--- src/main/java/id/co/imwizz/bolpax/controller/IssueController.java
+++ src/main/java/id/co/imwizz/bolpax/controller/IssueController.java
@@ -25,7 +25,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -33,7 +32,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
 @RestController
 @RequestMapping("/issue")
 public class IssueController {
